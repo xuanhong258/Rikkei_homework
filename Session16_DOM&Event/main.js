e6d781e1797b95027a339b7d5fefe399c87e9729
xuanhong258 <xuanhong258@gmail.com>
2024-04-17 23:18:43 +0700

    Update Session16_DOM&Event

--- Session16_DOM&Event/main.js
+++ Session16_DOM&Event/main.js
@@ -219,7 +219,6 @@
 
 // pressOperator()
 
-
 // nEqual.onclick = () => {
 //     let exp = output.innerText;
 //     let result;
@@ -231,138 +230,161 @@
 //     output.innerText = result;
 // }
 
-
 // nAc.onclick = () => {
 //     output.innerHTML = "0";
 // }
 
 //----------------------------Bai 08------------------------------
 
-// let nameLeft = document.querySelector(".content-left .content-row:nth-child(2) input")
-// let emailLeft = document.querySelector(".content-left .content-row:nth-child(3) input")
-// let phoneLeft = document.querySelector(".content-left .content-row:nth-child(4) input")
-// let addressLeft = document.querySelector(".content-left .content-row:nth-child(5) input")
-// let dayLeft = document.querySelector(".content-left .content-row:nth-child(6) input")
-// let payLeft = document.querySelector(".content-left .content-row:nth-child(7) input")
-// let checkboxLeft = document.querySelector(".content-left .content-checkbox input")
+let nameLeft = document.querySelector(
+  ".content-left .content-row:nth-child(2) input"
+);
+let emailLeft = document.querySelector(
+  ".content-left .content-row:nth-child(3) input"
+);
+let phoneLeft = document.querySelector(
+  ".content-left .content-row:nth-child(4) input"
+);
+let addressLeft = document.querySelector(
+  ".content-left .content-row:nth-child(5) input"
+);
+let dayLeft = document.querySelector(
+  ".content-left .content-row:nth-child(6) input"
+);
+let payLeft = document.querySelector(
+  ".content-left .content-row:nth-child(7) input"
+);
+let checkboxLeft = document.querySelector(
+  ".content-left .content-checkbox input"
+);
 
-// //------Thông tin người nhận-----------
-// let nameRight = document.querySelector(".content-right .content-row-right:nth-child(3) input");
-// let emailRight = document.querySelector(".content-right .content-row-right:nth-child(4) input");
-// let phoneRight = document.querySelector(".content-right .content-row-right:nth-child(5) input");
-// let addressRight = document.querySelector(".content-right .content-row-right:nth-child(6) input");
-// let checkboxRight = document.querySelector(".content-right .content-checkbox input")
-// if(nameLeft.value === ""){
-//     alert("Không được để trống họ và tên");
+//------Thông tin người nhận-----------
+let nameRight = document.querySelector(
+  ".content-right .content-row-right:nth-child(3) input"
+);
+let emailRight = document.querySelector(
+  ".content-right .content-row-right:nth-child(4) input"
+);
+let phoneRight = document.querySelector(
+  ".content-right .content-row-right:nth-child(5) input"
+);
+let addressRight = document.querySelector(
+  ".content-right .content-row-right:nth-child(6) input"
+);
+let checkboxRight = document.querySelector(
+  ".content-right .content-checkbox input"
+);
+if (nameLeft.value === "") {
+  alert("Không được để trống họ và tên");
+}
+if (emailLeft.value === "") {
+  alert("Không được để trống email");
+}
+//Check phone
+
+let regexPhone = /(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b/;
+
+if (!regexPhone.test(phoneLeft.value)) {
+  alert("Số điện thoại nhập chưa đúng định dạng");
+}
+if (phoneLeft.value === "") {
+  alert("Không được để trống ô số điện thoại");
+}
+
+//Check
+if (addressLeft.value === "") {
+  alert("Không được để trống ô địa chỉ");
+}
+if (dayLeft.value === "") {
+  alert("Không được để trống ô ngày đặt");
+}
+if (payLeft.value === "") {
+  alert("Không được để trống phương thức thanh toán");
+}
+//Check viết hoa họ và tên
+
+// if(nameLeft.value[0].charCodeAt() >= 97 && nameLeft.value[0].charCodeAt() <=122){
+//     alert("Phải viết hoa họ và tên");
 // }
-// if(emailLeft.value === ""){
-//     alert("Không được để trống email");
-// }
-// //Check phone
-
-// let regexPhone = /(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b/;
-
-// if(!regexPhone.test(phoneLeft.value)){
-//     alert("Số điện thoại nhập chưa đúng định dạng")
-// }
-// if(phoneLeft.value === ""){
-//     alert("Không được để trống ô số điện thoại");
-// }
-
-// //Check
-// if(addressLeft.value === ""){
-//     alert("Không được để trống ô địa chỉ");
-// }
-// if(dayLeft.value === ""){
-//     alert("Không được để trống ô ngày đặt");
-// }
-// if(payLeft.value === ""){
-//     alert("Không được để trống phương thức thanh toán");
-// }
-// //Check viết hoa họ và tên
-
-// // if(nameLeft.value[0].charCodeAt() >= 97 && nameLeft.value[0].charCodeAt() <=122){
-// //     alert("Phải viết hoa họ và tên");
-// // }
-// // let isCheck = false;
-// // for(let i = 1; i < nameLeft.value.length; i++){
-// //     if(nameLeft.value[i] === ' ' && nameLeft.value[i + 1].charCodeAt() >= 97 && nameLeft.value[i + 1].charCodeAt() <=122){
-// //         isCheck = true;
-// //     }
-// // }
-
-// let regexName = /^([A-Z][a-z]{1,})|(([A-Z][a-z]{1,}) ([A-Z][a-z]{1,}))$/;
-
-
-// console.log(regexName.test("Hong"));
-// // if(isCheck){
-// //     alert("Phải viết hoa họ và tên");
-// // }
-
-
-// //Check email;
-
-// let regexEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-// if(!regexEmail.test(emailLeft.value)){
-//     alert("Trường nhập email chưa chính xác");
-// }
-
-// //Check day
-
-// let regexDay = /(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[1,2])-(19|20)\d{2}/gm;
-
-// if(!regexDay.test(dayLeft.value)){
-//     alert("Ngày nhập chưa đúng định dạng");
-// }
-// //Check tôi là người nhận hàng
-// checkboxRight.onclick = () => {
-//     if(checkboxRight.checked){
-//         nameRight.value = nameLeft.value;
-//         emailRight.value = emailLeft.value;
-//         phoneRight.value = phoneLeft.value;
-//         addressRight.value = addressLeft.value; 
-//     }else{
-//         nameRight.value = "";
-//         emailRight.value = "";
-//         phoneRight.value = "";
-//         addressRight.value = "";
+// let isCheck = false;
+// for(let i = 1; i < nameLeft.value.length; i++){
+//     if(nameLeft.value[i] === ' ' && nameLeft.value[i + 1].charCodeAt() >= 97 && nameLeft.value[i + 1].charCodeAt() <=122){
+//         isCheck = true;
 //     }
 // }
 
+let regexName = /^[A-Z]*$/;
+let fullName = "Tran Xuan Hong";
+let tmp = fullName.split(" ");
 
+let tmp1 = tmp[0].split("");
 
-// let order = document.querySelector(".submit button:first-child");
-
-// checkboxLeft.onclick = () => {
-//     if(checkboxLeft.checked){
-//         order.onclick = () => {
-//             alert("Đặt hàng thành công, email đã gửi tới cho quý khách")
-//         }
-//     }else{
-//         order.onclick = () => {
-//             alert("Đặt hàng thành công");
-//         }
-//     }
-// } 
-
-// order.onclick = () => {
-//     if(!checkboxLeft.checked){
-//         alert("Đặt hàng thành công")
-//     }
+console.log(tmp1);
+// if(isCheck){
+//     alert("Phải viết hoa họ và tên");
 // }
 
-// let inputAgain = document.querySelector(".submit button:last-child");
+//Check email;
 
-// inputAgain.onclick = () => {
-//     nameLeft.value = "";
-//     emailLeft.value = "";
-//     phoneLeft.value = "";
-//     addressLeft.value = "";
-//     dayLeft.value = "";
-//     payLeft.value = "";
-// }
+let regexEmail =
+  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+if (!regexEmail.test(emailLeft.value)) {
+  alert("Trường nhập email chưa chính xác");
+}
+
+//Check day
+
+let regexDay = /(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[1,2])-(19|20)\d{2}/gm;
+
+if (!regexDay.test(dayLeft.value)) {
+  alert("Ngày nhập chưa đúng định dạng");
+}
+//Check tôi là người nhận hàng
+checkboxRight.onclick = () => {
+  if (checkboxRight.checked) {
+    nameRight.value = nameLeft.value;
+    emailRight.value = emailLeft.value;
+    phoneRight.value = phoneLeft.value;
+    addressRight.value = addressLeft.value;
+  } else {
+    nameRight.value = "";
+    emailRight.value = "";
+    phoneRight.value = "";
+    addressRight.value = "";
+  }
+};
+
+let order = document.querySelector(".submit button:first-child");
+
+checkboxLeft.onclick = () => {
+  if (checkboxLeft.checked) {
+    order.onclick = () => {
+      alert("Đặt hàng thành công, email đã gửi tới cho quý khách");
+    };
+  } else {
+    order.onclick = () => {
+      alert("Đặt hàng thành công");
+    };
+  }
+};
+
+order.onclick = () => {
+  if (!checkboxLeft.checked) {
+    alert("Đặt hàng thành công");
+  }
+};
+
+let inputAgain = document.querySelector(".submit button:last-child");
+
+inputAgain.onclick = () => {
+  nameLeft.value = "";
+  emailLeft.value = "";
+  phoneLeft.value = "";
+  addressLeft.value = "";
+  dayLeft.value = "";
+  payLeft.value = "";
+};
 
 //--------------Bai 09--------------
 // let iconLeft = document.querySelector(".icon-left");
@@ -422,90 +444,85 @@
 //   moveRight()
 // },1000)
 //---------------------Bai 10-----------------------------
-let images = document.querySelectorAll(".view .back-view");
+// let images = document.querySelectorAll(".view .back-view");
 
+// const cards = document.querySelectorAll(".card");
+// let matchCard = 0;
+// let cardOne, cardTwo;
+// let disableDeck = false;
 
-const cards = document.querySelectorAll(".card");
-let matchCard = 0;
-let cardOne, cardTwo;
-let disableDeck = false;
+// function flipCard(e){
+//   let clickedCard = e.target;
+//   if(clickedCard !== cardOne && !disableDeck){
+//       clickedCard.classList.add("flip");
+//       if(!cardOne){
+//         return cardOne = clickedCard;
+//       }
+//       cardTwo = clickedCard;
+//       disableDeck = true;
 
-function flipCard(e){
-  let clickedCard = e.target;
-  if(clickedCard !== cardOne && !disableDeck){
-      clickedCard.classList.add("flip");
-      if(!cardOne){
-        return cardOne = clickedCard;
-      }
-      cardTwo = clickedCard;
-      disableDeck = true;
-  
-      let cardOneImg = cardOne.querySelector("img");
-      let cardTwoImg = cardTwo.querySelector("img");
-      matchCards(cardOneImg,cardTwoImg);
-  }
-}
-
-cards.forEach(card => {
-  card.addEventListener("click",flipCard);
-})
-
-function matchCards(img1,img2){
-  if(img1.getAttribute("src") === img2.getAttribute("src")){
-    cardOne.removeEventListener("click", flipCard);
-    cardTwo.removeEventListener("click", flipCard);
-    cardOne = cardTwo = "";
-    disableDeck = false;
-    matchCard++;
-  }else{
-    setTimeout(() =>{
-      cardOne.classList.add("shake");
-      cardTwo.classList.add("shake");
-    },400)
-  
-    setTimeout(() => {
-      cardOne.classList.remove("shake","flip");
-      cardTwo.classList.remove("shake","flip");
-      cardOne = cardTwo = "";
-      disableDeck = false;
-    },1200)
-  }
-  if(matchCard === 8){
-    setTimeout(() => {
-      alert("Chúc mừng bạn!!!!!");
-      shuffleCard();
-    },400)
-  }
-}
-
-function shuffleCard() {
-  matchCard = 0;
-  cardOne = cardTwo = "";
-  let arr = [1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4];
-  arr.sort(() => Math.random() > 0.5 ? 1 : -1);
-// let  tmp = []
-// while(tmp.length < arr.length){
-//   let random = Math.floor(Math.random()*arr.length);
-//   if(tmp.indexOf(random) === -1){
-//     tmp.push(random);
+//       let cardOneImg = cardOne.querySelector("img");
+//       let cardTwoImg = cardTwo.querySelector("img");
+//       matchCards(cardOneImg,cardTwoImg);
 //   }
 // }
 
-// let newArr = []
-// for(let i = 0; i < tmp.length;i++){
-//   newArr.push(arr[tmp[i]]);
+// cards.forEach(card => {
+//   card.addEventListener("click",flipCard);
+// })
+
+// function matchCards(img1,img2){
+//   if(img1.getAttribute("src") === img2.getAttribute("src")){
+//     cardOne.removeEventListener("click", flipCard);
+//     cardTwo.removeEventListener("click", flipCard);
+//     cardOne = cardTwo = "";
+//     disableDeck = false;
+//     matchCard++;
+//   }else{
+//     setTimeout(() =>{
+//       cardOne.classList.add("shake");
+//       cardTwo.classList.add("shake");
+//     },400)
+
+//     setTimeout(() => {
+//       cardOne.classList.remove("shake","flip");
+//       cardTwo.classList.remove("shake","flip");
+//       cardOne = cardTwo = "";
+//       disableDeck = false;
+//     },1200)
+//   }
+//   if(matchCard === 8){
+//     setTimeout(() => {
+//       alert("Chúc mừng bạn!!!!!");
+//       shuffleCard();
+//     },400)
+//   }
 // }
-  cards.forEach((card,index) => {
-    card.classList.remove("flip");
-    let imgTag = card.querySelector("img");
-    imgTag.setAttribute("src",`./${arr[index]}.jpeg`);
-    card.addEventListener("click",flipCard);
-  })
-}
 
+// function shuffleCard() {
+//   matchCard = 0;
+//   cardOne = cardTwo = "";
+//   let arr = [1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4];
+//   arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+// // let  tmp = []
+// // while(tmp.length < arr.length){
+// //   let random = Math.floor(Math.random()*arr.length);
+// //   if(tmp.indexOf(random) === -1){
+// //     tmp.push(random);
+// //   }
+// // }
 
-
-
+// // let newArr = []
+// // for(let i = 0; i < tmp.length;i++){
+// //   newArr.push(arr[tmp[i]]);
+// // }
+//   cards.forEach((card,index) => {
+//     card.classList.remove("flip");
+//     let imgTag = card.querySelector("img");
+//     imgTag.setAttribute("src",`./${arr[index]}.jpeg`);
+//     card.addEventListener("click",flipCard);
+//   })
+// }
 
 // arr.sort(() => Math.random() > 0.5 ? 1 : -1);
 
